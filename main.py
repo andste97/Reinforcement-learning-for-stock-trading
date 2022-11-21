5e31db4fcb869d201453c01eba7b0424a734202b
lucab <lucabertolani98@gmail.com>
2022-11-21 15:54:39 +0100

    fixed bug in classical strategies

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if(__name__ == '__main__'):
     args = parser.parse_args()
     
     # Initialization of the required variables
-    run_config_path = "./configurations/hyperparameters-no-context.yml"
+    run_config_path = "./configurations/hyperparameters-default.yml"
     simulator = TradingSimulator(run_config_path)
     strategy = args.strategy
     stock = args.stock
@@ -43,7 +43,7 @@ if(__name__ == '__main__'):
     # simulator.analyseTimeSeries(stock)
     # simulator.simulateNewStrategy(strategy, stock)
     # simulator.simulateExistingStrategy(strategy, stock)
-    performanceTable = simulator.evaluateStrategy("TDQN")
+    performanceTable = simulator.evaluateStrategy("Buy and Hold")
     # simulator.evaluateStrategyOnStocks("TDRQN", ["Apple", "Google", "Volkswagen"])
     # simulator.evaluateStock(stock)
 
